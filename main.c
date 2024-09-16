4ee0758be9f858c043b92b11c289f5e8eb4b45e6
Rakoski <mastrakoski@gmail.com>
2024-09-16 20:56:23 -0300

    temperature conversion

--- main.c
+++ main.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    float fahr, celsius;
-    float lower, upper, step;
-    lower = 0;
-    upper = 300;
-    step = 20;
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32);
-        printf("%3f\t%6f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-
+int main() {
+  int fahr;
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
+  }
 }
